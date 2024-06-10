17cf6f5d508e2eeb937424b81afbc235c72f07a0
Sari727896 <hand1c17@mbjcomp.org.il>
2024-06-10 23:30:48 +0300

    Designing the singers

--- client-side/src/components/Singers.js
+++ client-side/src/components/Singers.js
@@ -1,18 +1,29 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchSingers } from '../redux/actions/singerAction';
+
 const style = {
-  borderRadius:'50%',
-  width: '100px', // אתה יכול לשנות את זה לפי הצורך שלך
-  height: '100px', // אתה יכול לשנות את זה לפי הצורך שלך
-  objectFit: 'cover', // לשמור על יחס הממדים של התמונה
-  marginRight: '10px', // רווח בין התמונה לטקסט
+  borderRadius: '50%',
+  width: '160px',
+  height: '160px',
+  objectFit: 'cover',
+  marginRight: '10px',
+};
+
+const gridContainerStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(auto-fit, minmax(150px, 1fr))',
+  gap: '20px',
+  justifyContent: 'center',
+};
+
+const itemStyle = {
+  textAlign: 'center',
 };
 
 export default function Singers() {
   const dispatch = useDispatch();
   const singers = useSelector((state) => state.singersReducer.singers); // Ensure correct state path
-  // debugger
 
   useEffect(() => {
     dispatch(fetchSingers());
@@ -23,17 +34,16 @@ export default function Singers() {
   }
 
   return (
-    
     <div>
       <h1>Singers</h1>
-      <ul>
+      <div style={gridContainerStyle}>
         {singers.map((singer) => (
-          <li key={singer.id}>
-            <img src={singer.image}  style={style} alt={singer.firstName} />
-            <h3> {singer.firstName} {singer.lastName}</h3>
-          </li>
+          <div key={singer.id} style={itemStyle}>
+            <img src={singer.image} style={style} alt={singer.firstName} />
+            <h3>{singer.firstName} {singer.lastName}</h3>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
